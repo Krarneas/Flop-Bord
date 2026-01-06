94fb6b2847af1752dcb20149fe389cd31e346c68
Krarneas <af.saulon@gmail.com>
2026-01-06 13:23:35 +0800

    feat/add-player-movement
    - when space is pressed, jump must be performed
    - to note: did not use Unity's new Input System (deprecated)

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //makes the script aware of the Rigidbody2D component attached to the bird game object
+    public Rigidbody2D myRigidbody; 
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //when space is pressed do:
+        if (Input.GetKeyDown(KeyCode.Space) == true) //deprecated
+        {
+            //sets the vertical velocity of the bird to 10 units per second upwards
+            myRigidbody.linearVelocity = Vector2.up * 10f;
+        }
+
+
     }
 }
